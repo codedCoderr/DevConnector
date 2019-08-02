f1b3ca262191cb345aa3eb04a10a34a3e5af8591
Busola <olowubusolaolutayo1992@gmail.com>
2019-08-02 21:56:33 +0100

    refactor midleware

--- server/middleware/index.js
+++ server/middleware/index.js
@@ -1,17 +1,17 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = async (req, res, next) => {
-  try {
-    const jwtsecret = process.env.jwtsecret;
-    const token = req.header('x-auth-token');
+  const jwtsecret = process.env.jwtsecret;
+  const token = req.header('x-auth-token');
 
-    if (!token) {
-      return res.status(401).json('No token, authorization denied');
-    }
+  if (!token) {
+    return res.status(401).json({msg:'No token, authorization denied'});
+  }
+  try {
     const decoded = jwt.verify(token, jwtsecret);
     req.user = decoded.user;
     next();
   } catch (error) {
-    res.status(401).json('Token is not valid');
+    res.status(401).json({msg:'Token is not valid'});
   }
 };
