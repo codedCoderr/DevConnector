cf952e7e9802d5aad53b551e0ed22fb93ccc0a87
Busola <olowubusolaolutayo1992@gmail.com>
2019-08-02 21:55:54 +0100

    server setup

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const cors = require('cors');
+app.use(cors());
+require('dotenv').config();
+
+app.use(express.json({ extended: false }));
+
+const db = process.env.localdb;
+mongoose.connect(db, { useNewUrlParser: true });
+
+app.use(require('./routes/auth'));
+app.use(require('./routes/post'));
+app.use(require('./routes/profile'));
+
+mongoose.set('useCreateIndex', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useNewUrlParser', true);
+
+const PORT = process.env.PORT || 3500;
+app.listen(PORT, () => {
+  console.log('Server Connected');
+});
